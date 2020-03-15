d11be03ea122aac78c6dee9b05388aa3d9b95d32
Tugkan Cengiz <tugkancengiz@gmail.com>
2020-03-15 12:49:38 +0300

    ready for prod

---
+++ README.md
@@ -0,0 +1,7 @@
+# Coronavirus stats in the Singapore
+Actor gets an actual number of infected, recovered and deceased people in the Singapore from https://www.moh.gov.sg/covid-19
+
+Latest data are available at this URL: https://api.apify.com/v2/key-value-stores/3Po6TV7wTht4vIEid/records/LATEST?disableRedirect=true
+You can find a dataset with unique history items here https://api.apify.com/v2/datasets/L3VCmhMeX0KUQeJto/items?format=json&clean=1
+
+URL is being actualized every 5 minutes.
---
+++ src/main.js
@@ -0,0 +1,62 @@
+const Apify = require('apify');
+
+const { log } = Apify.utils;
+const sourceUrl = 'https://www.moh.gov.sg/covid-19';
+const LATEST = 'LATEST';
+
+Apify.main(async () => {
+    const requestQueue = await Apify.openRequestQueue();
+    const kvStore = await Apify.openKeyValueStore('COVID-19-SINGAPORE');
+    const dataset = await Apify.openDataset('COVID-19-SINGAPORE-HISTORY');
+
+    await requestQueue.addRequest({ url: sourceUrl });
+    const crawler = new Apify.CheerioCrawler({
+        requestQueue,
+        useApifyProxy: true,
+        apifyProxyGroups: ['SHADER'],
+        handlePageTimeoutSecs: 60 * 2,
+        handlePageFunction: async ({ $ }) => {
+            log.info('Page loaded.');
+            const now = new Date();
+
+            const stableHospitalized = parseInt($($($('table[class=""]:not([border]) tr').get(3)).find('td').get(0)).text().trim(), 10);
+            const criticalHospitalized = parseInt($($($('table[class=""]:not([border]) tr').get(3)).find('td').get(1)).text().trim(), 10);
+            const deaths = parseInt($($($('table[class=""]:not([border]) tr').get(5)).find('td').get(0)).text().trim(), 10);
+            const discharged = parseInt($($($('table[class=""]:not([border]) tr').get(5)).find('td').get(1)).text().trim(), 10);
+
+
+            const data = {
+                infected: stableHospitalized + criticalHospitalized,
+                deceased: deaths,
+                recovered: discharged,
+                sourceUrl,
+                lastUpdatedAtApify: new Date(Date.UTC(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours() + 1, now.getMinutes())).toISOString(),
+                readMe: 'https://apify.com/tugkan/covid-sg',
+            };
+
+            // Compare and save to history
+            const latest = await kvStore.getValue(LATEST) || {};
+            delete latest.lastUpdatedAtApify;
+            const actual = Object.assign({}, data);
+            delete actual.lastUpdatedAtApify;
+
+            if (JSON.stringify(latest) !== JSON.stringify(actual)) {
+                log.info('Data did change :( storing new to dataset.');
+                await dataset.pushData(data);
+            }
+
+            await kvStore.setValue(LATEST, data);
+            log.info('Data stored, finished.');
+        },
+
+        // This function is called if the page processing failed more than maxRequestRetries+1 times.
+        handleFailedRequestFunction: async ({ request }) => {
+            console.log(`Request ${request.url} failed twice.`);
+        },
+    });
+
+    // Run the crawler and wait for it to finish.
+    await crawler.run();
+
+    console.log('Crawler finished.');
+});
